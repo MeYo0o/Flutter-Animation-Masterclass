e0b0881d47aaa63644aa45dc7e4a64c5da9f48c8
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-04-06 10:50:21 +0200

    Finish EP8

--- lib/pages/tween_animation_builder_page.dart
+++ lib/pages/tween_animation_builder_page.dart
@@ -31,47 +31,51 @@ class _HSVColorSelectorState extends State<HSVColorSelector> {
       mainAxisSize: MainAxisSize.min,
       children: [
         AnimatedContainer(
-          color: HSVColor.fromAHSV(1.0, _hue, 1.0, 1.0).toColor(),
           duration: const Duration(milliseconds: 1500),
-          width: 200,
           height: 200,
+          width: 200,
+          color: HSVColor.fromAHSV(1.0, _hue, 1.0, 1.0).toColor(),
         ),
-        const SizedBox(height: 48.0),
+        const SizedBox(height: 40),
         TweenAnimationBuilder<double>(
           tween: Tween<double>(
             begin: 0.0,
             end: _hue,
           ),
           duration: const Duration(milliseconds: 1500),
-          builder: (context, hue, child) {
-            final hsvColor = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0);
+          builder: (context, hueValue, child) {
+            final hsvColor = HSVColor.fromAHSV(1.0, hueValue, 1.0, 1.0);
             return Container(
-              width: 200,
               height: 200,
+              width: 200,
               color: hsvColor.toColor(),
             );
           },
         ),
-        const SizedBox(height: 48.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Container(
-            height: 30.0,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [for (var h = 0.0; h < 360.0; h += 1) HSVColor.fromAHSV(1.0, h, 1.0, 1.0).toColor()],
-                stops: [
-                  for (var h = 0.0; h < 360.0; h += 1) h / 360.0,
-                ],
-              ),
+        const SizedBox(height: 20),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          height: 30,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                for (double i = 0; i <= 360; i++)
+                  HSVColor.fromAHSV(1.0, i, 1.0, 1.0).toColor()
+              ],
+              stops: [for (double i = 0; i <= 360; i++) i / 360],
             ),
           ),
         ),
-        Slider.adaptive(
+        Slider(
+          min: 0.0,
+          max: 360.0,
           value: _hue,
-          min: 0,
-          max: 360,
-          onChanged: (newValue) => setState(() => _hue = newValue),
+          onChanged: (double newValue) {
+            setState(() {
+              _hue = newValue;
+            });
+          },
         ),
       ],
     );
