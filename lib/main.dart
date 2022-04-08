9f3d50f9618ef17cc885b360cc883f2772b9886f
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-04-08 21:29:39 +0200

    Add The StopWatch Project Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,121 @@
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'pages/ep1_to_ep10/animated_container_page.dart';
+// import 'pages/ep1_to_ep10/animated_opacity_page.dart';
+// import 'pages/ep1_to_ep10/tween_animation_builder_page.dart';
+//
+// enum AnimationExample {
+//   animatedContainer,
+//   animatedOpacity,
+//   tweenAnimationBuilder,
+// }
+//
+// extension on AnimationExample {
+//   String capitalizeFirstCharacter() {
+//     final name = describeEnum(this);
+//     return name.replaceRange(0, 1, name.characters.first.toUpperCase());
+//   }
+// }
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.indigo,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('Animations Playground')),
+//         body: const AnimationExamplesList(),
+//       ),
+//       onGenerateRoute: (settings) {
+//         switch (settings.name) {
+//           case 'animatedContainer':
+//             return MaterialPageRoute(
+//                 builder: (_) => const AnimatedContainerPage());
+//           case 'animatedOpacity':
+//             return MaterialPageRoute(
+//                 builder: (_) => const AnimatedOpacityPage());
+//           case 'tweenAnimationBuilder':
+//             return MaterialPageRoute(
+//                 builder: (_) => const TweenAnimationBuilderPage());
+//           default:
+//             throw UnimplementedError(
+//                 'Route ${settings.name} not implemented');
+//         }
+//       },
+//     );
+//   }
+// }
+//
+// class AnimationExamplesList extends StatelessWidget {
+//   const AnimationExamplesList({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemBuilder: (context, index) {
+//         final example = AnimationExample.values[index];
+//         final routeName = describeEnum(example);
+//         debugPrint(routeName);
+//         return ListTile(
+//           title: Text(example.capitalizeFirstCharacter()),
+//           trailing: const Icon(Icons.chevron_right),
+//           onTap: () => Navigator.of(context).pushNamed(routeName),
+//         );
+//       },
+//       itemCount: AnimationExample.values.length,
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'pages/animated_container_page.dart';
-import 'pages/animated_opacity_page.dart';
-import 'pages/tween_animation_builder_page.dart';
-
-enum AnimationExample {
-  animatedContainer,
-  animatedOpacity,
-  tweenAnimationBuilder,
-}
-
-extension on AnimationExample {
-  String capitalizeFirstCharacter() {
-    final name = describeEnum(this);
-    return name.replaceRange(0, 1, name.characters.first.toUpperCase());
-  }
-}
+import 'package:flutter/services.dart';
+import 'package:flutter_animations_masterclass/pages/ep11_to_ep/stopwatch.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Animations Playground')),
-        body: const AnimationExamplesList(),
-      ),
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case 'animatedContainer':
-            return MaterialPageRoute(
-                builder: (_) => const AnimatedContainerPage());
-          case 'animatedOpacity':
-            return MaterialPageRoute(
-                builder: (_) => const AnimatedOpacityPage());
-          case 'tweenAnimationBuilder':
-            return MaterialPageRoute(
-                builder: (_) => const TweenAnimationBuilderPage());
-          default:
-            throw UnimplementedError(
-                'Route ${settings.name} not implemented');
-        }
-      },
+      home: MyHomePage(),
     );
   }
 }
 
-class AnimationExamplesList extends StatelessWidget {
-  const AnimationExamplesList({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        final example = AnimationExample.values[index];
-        final routeName = describeEnum(example);
-        debugPrint(routeName);
-        return ListTile(
-          title: Text(example.capitalizeFirstCharacter()),
-          trailing: const Icon(Icons.chevron_right),
-          onTap: () => Navigator.of(context).pushNamed(routeName),
-        );
-      },
-      itemCount: AnimationExample.values.length,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: AspectRatio(
+              aspectRatio: 0.85,
+              child: MeYoStopWatch(),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
