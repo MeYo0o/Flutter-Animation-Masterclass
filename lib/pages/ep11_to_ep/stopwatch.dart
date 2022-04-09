8c6bf6f6c114002d87725106a8f4c118dd3244ca
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-04-09 17:07:47 +0200

    Add EP13

--- lib/pages/ep11_to_ep/stopwatch.dart
+++ lib/pages/ep11_to_ep/stopwatch.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_animations_masterclass/pages/ep11_to_ep/elapsed_time_text_basic.dart';
+import 'package:flutter_animations_masterclass/pages/ep11_to_ep/elapsed_time_text.dart';
 
 class MeYoStopWatch extends StatefulWidget {
   const MeYoStopWatch({Key? key}) : super(key: key);
@@ -33,7 +33,7 @@ class _MeYoStopWatchState extends State<MeYoStopWatch> {
 
   @override
   Widget build(BuildContext context) {
-    return ElapsedTimeTextBasic(
+    return ElapsedTimeText(
       elapsed: _elapsed,
     );
   }
